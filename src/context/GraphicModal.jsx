683d3c75e2a5c25f6ffb625d7e3d79d33857e215
oneaxissolutions <oneaxissolutions@email.com>
2026-03-21 17:57:42 +0530

    Build fix

--- src/context/GraphicModal.jsx
+++ src/context/GraphicModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
-import '../css/GraphicModal.css';
+import '../css/Graphicmodal.css';
 
 const GraphicModal = ({ isOpen, onClose }) => {
 
